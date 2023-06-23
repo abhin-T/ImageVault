4fd2ef37ec3fecebed1f436050e57326802fceac
abhinT <abhintomar1@gmail.com>
2023-06-22 20:12:38 -0400

    Added some styling to elements

--- imagevault/src/App.jsx
+++ imagevault/src/App.jsx
@@ -2,22 +2,27 @@ import React, { useState, useEffect } from 'react';
 import Login from './components/login/Login.jsx'
 import Menu from './components/menu/Menu.jsx'
 import { auth } from './firebase.js'
+import { updatePhoneNumber } from 'firebase/auth';
 
 const App = () => {
 
   const [user, setUser] = useState(undefined)
 
+  // sets up listener once component mounts
   useEffect(() =>
   {
+    // checks for change in state of user
     auth.onAuthStateChanged(user =>
     {
       setUser(user && user.uid ? user : null)
     })
   }, [])
 
-  if ( undefined === user )
+  // loading animation while getting user data 
+  if ( user === undefined )
     return <h1>Loading...</h1>
 
+  // user is logged in
   if ( user != null )
     return (<Menu email={user.email}/>)
 
--- imagevault/src/components/login/Login.jsx
+++ imagevault/src/components/login/Login.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import { signIn } from '../../firebase.js'
+import { Container, Row, Col } from 'react-bootstrap'
+import './login.css'
 
 const Login = () => {
   return (
-    <div>
-      <h1>Login</h1>
-      <button onClick={signIn}>Sign In</button></div>
+    <Container fluid className='text-center justify-content-center align-items-center h-100'>
+      <Row className='d-flex align-items-center h-100 pb-5'>
+        <Col>
+          <h1 className='pb-3'>ImageVault</h1>
+          <button type="button" className="login-with-google-btn" onClick={signIn} >Sign in with Google</button>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
